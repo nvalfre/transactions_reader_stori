2459c530887e1f1f0ce236dbfcc17a0ab3d80187
nvalfre <kokevalfre@gmail.com>
2023-05-22 22:54:37 -0300

    sqlite

--- repository/account_repository.go
+++ repository/account_repository.go
@@ -1,10 +1,21 @@
 package repository
 
 import (
+	"log"
 	"transactions_reader_stori/domain/dao"
 )
 
 // SaveAccount saves an account to the database
 func (r *DatabaseRepo) SaveAccount(account *dao.Account) error {
-	return r.Db.Create(account).Error
+	// Insert the account into the database
+	result, err := r.Db.Exec("INSERT INTO accounts (name, email, balance) VALUES (?, ?)", account.Name, account.Email, account.Balance)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	// Get the inserted account ID
+	accountID, _ := result.LastInsertId()
+	account.ID = uint(int(accountID))
+	return nil
 }
--- repository/transaction_repository.go
+++ repository/transaction_repository.go
@@ -1,10 +1,19 @@
 package repository
 
 import (
+	"log"
 	"transactions_reader_stori/domain/dao"
 )
 
 // SaveTransaction saves a transaction_service to the database
 func (r *DatabaseRepo) SaveTransaction(transaction *dao.Transaction) error {
-	return r.Db.Create(transaction).Error
+	result, err := r.Db.Exec("INSERT INTO transactions (account_id, date, amount, is_credit) VALUES (?, ?, ?, ?)", transaction.AccountID, transaction.Date, transaction.Amount, transaction.IsCredit)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	transactionID, _ := result.LastInsertId()
+	transaction.ID = uint(int(transactionID))
+	return nil
 }
