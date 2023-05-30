e001acc8813e7d757cf4954fee77de5eb5a1ce52
nvalfre <kokevalfre@gmail.com>
2023-05-29 22:59:26 -0300

    Adjust body style fmt

--- services/email_service/body.go
+++ services/email_service/body.go
@@ -8,6 +8,15 @@ import (
 	"transactions_reader_stori/domain"
 )
 
+const (
+	sectionTitle      = "Total Balance"
+	sectionColor      = "#3366CC"
+	contentColor      = "#333333"
+	tableHeaderColor  = "#CCCCCC"
+	tableContentColor = "#FFFFFF"
+	textColor         = "#000000"
+)
+
 func (s *EmailService) buildEmailBodyContent(summary *domain.SummaryVO) string {
 	sort.Slice(summary.TransactionSummary, func(i, j int) bool {
 		return summary.TransactionSummary[i].Month.Before(summary.TransactionSummary[j].Month)
@@ -17,7 +26,9 @@ func (s *EmailService) buildEmailBodyContent(summary *domain.SummaryVO) string {
 	sbPointer := &sb
 
 	sbPointer.WriteString("<html>")
-	s.buildHeader(sbPointer)
+	sbPointer.WriteString("<head>")
+	sbPointer.WriteString(`<meta name="viewport" content="width=device-width, initial-scale=1.0">`)
+	sbPointer.WriteString("</head>")
 	s.buildBody(summary, sbPointer)
 	sbPointer.WriteString("</html>")
 
@@ -35,90 +46,74 @@ func (s *EmailService) buildBodyContainer(summary *domain.SummaryVO, sb *strings
 	sb.WriteString(`<div class="container">`)
 	s.buildAppbarHeader(sb)
 
-	s.buildTotalBalanceSection(summary, sb)
-
-	s.buildTransactionSummarySection(summary, sb)
-
-	s.buildAverageCreditAmountSection(summary, sb)
-
-	s.buildAverageDebitAmountSection(summary, sb)
-
-	s.buildMonthlySummary(summary, sb)
+	s.buildTotalBalanceSection(summary, sb, sectionTitle, sectionColor, contentColor)
+	s.buildTransactionSummarySection(summary, sb, sectionTitle, sectionColor, contentColor)
+	s.buildAverageDebitAmountSection(summary, sb, sectionTitle, sectionColor, contentColor)
+	s.buildAverageCreditAmountSection(summary, sb, sectionTitle, sectionColor, contentColor)
+	s.buildMonthlySummary(summary, sb, sectionColor, tableHeaderColor, tableContentColor, textColor)
 
 	sb.WriteString(`</div>`)
-}
-
-func (s *EmailService) buildMonthlySummary(summary *domain.SummaryVO, sb *strings.Builder) {
-	for month, monthlySummary := range summary.MonthlySummary {
-		sb.WriteString(`<div class="section">`)
-		sb.WriteString(fmt.Sprintf(`<h2>Monthly Summary for %s</h2>`, month.String()))
-		sb.WriteString(`<table class="summary-table">`)
-		sb.WriteString(`<tr>`)
-		sb.WriteString(`<th>Month</th>`)
-		sb.WriteString(`<th>Transaction ID</th>`)
-		sb.WriteString(`<th>Amount</th>`)
-		sb.WriteString(`</tr>`)
-		for _, ts := range monthlySummary.Summaries {
-			sb.WriteString(`<tr>`)
-			sb.WriteString(fmt.Sprintf(`<td>%s</td>`, month.String()))
-			sb.WriteString(fmt.Sprintf(`<td>%d</td>`, ts.ID))
-			sb.WriteString(fmt.Sprintf(`<td>%.2f</td>`, ts.Amount))
-			sb.WriteString(`</tr>`)
-		}
-		sb.WriteString(`</table>`)
-		sb.WriteString(`</div>`)
-	}
-}
-
-func (s *EmailService) buildHeader(sb *strings.Builder) {
-	sb.WriteString("<head>")
-	s.loadStyle(sb)
-	sb.WriteString("</head>")
-}
-
-func (s *EmailService) loadStyle(sb *strings.Builder) {
-	sb.WriteString("<style>")
-	sb.WriteString(style)
-	sb.WriteString("</style>")
 }
 
 func (s *EmailService) buildAppbarHeader(sb *strings.Builder) {
-	sb.WriteString(`<h1>Summary Report</h1>`)
+	sb.WriteString(`<h1 style="color: #333333;">Summary Report</h1>`)
 }
 
-func (s *EmailService) buildTotalBalanceSection(summary *domain.SummaryVO, sb *strings.Builder) {
+func (s *EmailService) buildTotalBalanceSection(summary *domain.SummaryVO, sb *strings.Builder, sectionTitle, sectionColor, contentColor string) {
 	sb.WriteString(`<div class="section">`)
-	sb.WriteString(`<h2>Total Balance</h2>`)
-	sb.WriteString(fmt.Sprintf(`<p>%.2f</p>`, summary.TotalBalance))
+	sb.WriteString(fmt.Sprintf(`<h2 style="color: %s;">%s</h2>`, sectionColor, sectionTitle))
+	sb.WriteString(fmt.Sprintf(`<p style="color: %s;">%.2f</p>`, contentColor, summary.TotalBalance))
 	sb.WriteString(`</div>`)
 }
 
-func (s *EmailService) buildTransactionSummarySection(summary *domain.SummaryVO, sb *strings.Builder) {
+func (s *EmailService) buildTransactionSummarySection(summary *domain.SummaryVO, sb *strings.Builder, sectionTitle, sectionColor, contentColor string) {
 	sb.WriteString(`<div class="section">`)
-	sb.WriteString(`<h2>Transaction Summaries</h2>`)
+	sb.WriteString(fmt.Sprintf(`<h2 style="color: %s;">%s</h2>`, sectionColor, sectionTitle))
 	prevMonth := time.Time{}
 	for _, ts := range summary.TransactionSummary {
 		if !ts.Month.Equal(prevMonth) {
-			sb.WriteString(fmt.Sprintf(`<h3>%s</h3>`, ts.Month.Format("January")))
+			sb.WriteString(fmt.Sprintf(`<h3 style="color: %s;">%s</h3>`, contentColor, ts.Month.Format("January")))
 			prevMonth = ts.Month
 		}
 		sb.WriteString(`<ul><li>`)
-		sb.WriteString(fmt.Sprintf(`Transaction ID: %d, Amount: %.2f`, ts.ID, ts.Amount))
+		sb.WriteString(fmt.Sprintf(`<span style="color: %s;">Transaction ID: %d, Amount: %.2f</span>`, contentColor, ts.ID, ts.Amount))
 		sb.WriteString(`</li></ul>`)
 	}
 	sb.WriteString(`</div>`)
 }
 
-func (s *EmailService) buildAverageDebitAmountSection(summary *domain.SummaryVO, sb *strings.Builder) {
+func (s *EmailService) buildAverageDebitAmountSection(summary *domain.SummaryVO, sb *strings.Builder, sectionTitle, sectionColor, contentColor string) {
 	sb.WriteString(`<div class="section">`)
-	sb.WriteString(`<h2>Average Debit Amount</h2>`)
-	sb.WriteString(fmt.Sprintf(`<p>%.2f</p>`, summary.AverageDebit))
+	sb.WriteString(fmt.Sprintf(`<h2 style="color: %s;">%s</h2>`, sectionColor, sectionTitle))
+	sb.WriteString(fmt.Sprintf(`<p style="color: %s;">%.2f</p>`, contentColor, summary.AverageDebit))
 	sb.WriteString(`</div>`)
 }
 
-func (s *EmailService) buildAverageCreditAmountSection(summary *domain.SummaryVO, sb *strings.Builder) {
+func (s *EmailService) buildAverageCreditAmountSection(summary *domain.SummaryVO, sb *strings.Builder, sectionTitle, sectionColor, contentColor string) {
 	sb.WriteString(`<div class="section">`)
-	sb.WriteString(`<h2>Average Credit Amount</h2>`)
-	sb.WriteString(fmt.Sprintf(`<p>%.2f</p>`, summary.AverageCredit))
+	sb.WriteString(fmt.Sprintf(`<h2 style="color: %s;">%s</h2>`, sectionColor, sectionTitle))
+	sb.WriteString(fmt.Sprintf(`<p style="color: %s;">%.2f</p>`, contentColor, summary.AverageCredit))
 	sb.WriteString(`</div>`)
+}
+
+func (s *EmailService) buildMonthlySummary(summary *domain.SummaryVO, sb *strings.Builder, sectionColor, tableHeaderColor, tableContentColor, textColor string) {
+	for month, monthlySummary := range summary.MonthlySummary {
+		sb.WriteString(`<div class="section">`)
+		sb.WriteString(fmt.Sprintf(`<h2>Monthly Summary for %s</h2>`, month.String()))
+		sb.WriteString(`<table class="summary-table">`)
+		sb.WriteString(`<tr>`)
+		sb.WriteString(fmt.Sprintf(`<th style="background-color: %s; color: %s;">Month</th>`, tableHeaderColor, textColor))
+		sb.WriteString(fmt.Sprintf(`<th style="background-color: %s; color: %s;">Transaction ID</th>`, tableHeaderColor, textColor))
+		sb.WriteString(fmt.Sprintf(`<th style="background-color: %s; color: %s;">Amount</th>`, tableHeaderColor, textColor))
+		sb.WriteString(`</tr>`)
+		for _, ts := range monthlySummary.Summaries {
+			sb.WriteString(`<tr>`)
+			sb.WriteString(fmt.Sprintf(`<td style="background-color: %s; color: %s;">%s</td>`, tableContentColor, textColor, month.String()))
+			sb.WriteString(fmt.Sprintf(`<td style="background-color: %s; color: %s;">%d</td>`, tableContentColor, textColor, ts.ID))
+			sb.WriteString(fmt.Sprintf(`<td style="background-color: %s; color: %s;">%.2f</td>`, tableContentColor, textColor, ts.Amount))
+			sb.WriteString(`</tr>`)
+		}
+		sb.WriteString(`</table>`)
+		sb.WriteString(`</div>`)
+	}
 }
